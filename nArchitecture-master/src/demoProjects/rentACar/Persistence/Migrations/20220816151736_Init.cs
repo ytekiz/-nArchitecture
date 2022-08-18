344fd8b5aaf9f216a73955435193ebdd498c775e
Yaşar <64525859+ytekiz@users.noreply.github.com>
2022-08-18 22:58:17 +0300

    RentACar Projesi
    Engin Demiroğ RentACar projesi ilk ders tamamlandı

---
+++ nArchitecture-master/src/demoProjects/rentACar/Persistence/Migrations/20220816151736_Init.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Brans",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Brans", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Brans",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "BMW" });
+
+            migrationBuilder.InsertData(
+                table: "Brans",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Mercesdes" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Brans");
+        }
+    }
+}
